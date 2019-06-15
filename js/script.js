9b226785416347c3d26069f6336fea7b43c78116
Aniketh <anikethnair97@gmail.com>
2019-06-15 21:09:21 +0530

    Menu Update

--- js/script.js
+++ js/script.js
@@ -20,82 +20,112 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 });
 
 (function (global) {
-	var dc = {};
 
-	var homeHtml = "snippets/home-snippet.html";
-	var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
-	var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-	var categoryHtml = "snippets/category-snippet.html"
+var dc = {};
 
-	//Fuction for inserting innerHtml for 'select'
-	var insertHtml = function (selector, html) {
-		var targetElem = document.querySelector(selector);
-		targetElem.innerHtml = html;
-	};
+var homeHtml = "snippets/home-snippet.html";
+var allCategoriesUrl =
+  "https://davids-restaurant.herokuapp.com/categories.json";
+var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+var categoryHtml = "snippets/category-snippet.html";
 
-	//Show  loading icon inside element identified by 'selector'
-	var showLoading = function (selector) {
-		var html = "<div class='text-center'>";
-		html += "<img src='images/ajax-loader.gif'></div>";
-		insertHtml(selector, html);
-	};
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
 
-	//Return substitue of '{{propName}}'
-	//with propValue in given 'string'
-	var insertProperty = function (string, propName, propValue){
-		var propToReplace = "{{" + propName + "}}";
-		string = string.replace(new RegExp(propToReplace, "g"), propValue);
-		return string;
-	}
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
 
-	//On page load (before images or css)
-	document.addEventListener("DOMContentLoaded", function(event){
+// Return substitute of '{{propName}}'
+// with propValue in given 'string'
+var insertProperty = function (string, propName, propValue) {
+  var propToReplace = "{{" + propName + "}}";
+  string = string
+    .replace(new RegExp(propToReplace, "g"), propValue);
+  return string;
+}
 
-		//On first load, show home view
-		showLoading("#main-content");
-		$ajaxUtils.sendGetRequest(homeHtml, function (responseText){
-			document.querySelector("#main-content").innerHtml = responseText;
-		}, false);
-	});
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
 
-	//Load the menu categories view
-	dc.loadMenuCategories = function (){
-		showLoading("#main-content");
-		$ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML);
-	};
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
+});
 
-	//Builds HTML for the categories page based on the data
-	//from the server
-	function buildAndShowCategoriesHTML (categories) {
-		//Load title snippet of categories page
-		$ajaxUtils.sendGetRequest(categoriesTitleHtml, function (categoriesTitleHtml){
-			//Retrieve single category snippet
-			$ajaxUtils.sendGetRequest(categoryHtml, function (categoryHtml){
-			var categoriesViewHtml = buildCategoriesViewHtml (categories, categoriesTitleHtml, categoryHtml);
-			insertHtml("#main-content", categoriesViewHtml);
-			}, false)
-			
-		}, false);
-	}
+// Load the menu categories view
+dc.loadMenuCategories = function () {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(
+    allCategoriesUrl,
+    buildAndShowCategoriesHTML);
+};
 
-	//Using categories data and snippets html
-	//build categories view html to be inserted into page
-	function buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml){
-		var finalHtml = categoriesategoriesTitleHtml;
-		finalHtml += "<section class='row'";
 
-		//Loop over catgories
-		for (var i=0; i <categories.length; i++){
-			//Insert category values
-			var html = categoryHtml;
-			var name = "" + categories[i].name;
-			var short_name = categories[i].short_name;
-			html = insertProperty(html, "name", name);
-			html = insertProperty(html, "short_name", short_name);
-			finalHtml +=html;
-		}
-		finalHtml +="</section>";
-		return finalHtml;
-	}
-	global.$dc = dc;
+// Builds HTML for the categories page based on the data
+// from the server
+function buildAndShowCategoriesHTML (categories) {
+  // Load title snippet of categories page
+  $ajaxUtils.sendGetRequest(
+    categoriesTitleHtml,
+    function (categoriesTitleHtml) {
+      // Retrieve single category snippet
+      $ajaxUtils.sendGetRequest(
+        categoryHtml,
+        function (categoryHtml) {
+          var categoriesViewHtml =
+            buildCategoriesViewHtml(categories,
+                                    categoriesTitleHtml,
+                                    categoryHtml);
+          insertHtml("#main-content", categoriesViewHtml);
+        },
+        false);
+    },
+    false);
+}
+
+
+// Using categories data and snippets html
+// build categories view HTML to be inserted into page
+function buildCategoriesViewHtml(categories,
+                                 categoriesTitleHtml,
+                                 categoryHtml) {
+
+  var finalHtml = categoriesTitleHtml;
+  finalHtml += "<section class='row'>";
+
+  // Loop over categories
+  for (var i = 0; i < categories.length; i++) {
+    // Insert category values
+    var html = categoryHtml;
+    var name = "" + categories[i].name;
+    var short_name = categories[i].short_name;
+    html =
+      insertProperty(html, "name", name);
+    html =
+      insertProperty(html,
+                     "short_name",
+                     short_name);
+    finalHtml += html;
+  }
+
+  finalHtml += "</section>";
+  return finalHtml;
+}
+
+
+global.$dc = dc;
+
 })(window);
